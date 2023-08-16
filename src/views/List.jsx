234dbed018232de0f399c83dc63922cc5a7f6ef1
Aslan Taheri <fromthenarnia@gmail.com>
2023-08-15 20:58:35 -0400

    remove the TODO comment.

--- src/views/List.jsx
+++ src/views/List.jsx
@@ -10,11 +10,6 @@ export function List({ data }) {
 				{data.map((item) => (
 					<ListItem key={item.id} name={item.name} />
 				))}
-				{/**
-				 * TODO: write some JavaScript that renders the `data` array
-				 * using the `ListItem` component that's imported at the top
-				 * of this file.
-				 */}
 			</ul>
 		</>
 	);
