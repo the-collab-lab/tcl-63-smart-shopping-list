65845e06a5745802a215e34203920227a818dc55
Aslan Taheri <fromthenarnia@gmail.com>
2023-08-22 19:18:14 -0400

    Create Home component to retrieve token or generate a new token.

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,11 +1,61 @@
+import { generateToken } from '@the-collab-lab/shopping-list-utils';
+import Button from '../components/Button';
 import './Home.css';
+import { useState } from 'react';
+import { createCollection } from '../api/firebase';
 
-export function Home() {
+export function Home({ setListToken }) {
+	const [retrievedToken, setRetrievedToken] = useState('');
+
+	/**
+	 * This function generates a new unique Token corresponding to a new shopping list.
+	 */
+	const createNewList = () => {
+		const newToken = generateToken();
+		setRetrievedToken(newToken);
+
+		/**
+		 * We were creating a new collection in the database using the newToken:
+		 * createCollection(newToken);
+		 */
+	};
+
+	/**
+	 *  This function ensures that the retrievedToken state always reflects the current value in the input field.
+	 */
+	const handleTokenInput = (e) => {
+		setRetrievedToken(e.target.value);
+	};
+
+	/**
+	 * This function attempts to join a shopping list based on the provided Token.
+	 */
+	const joinExistentList = (e) => {
+		e.preventDefault();
+		setListToken(retrievedToken);
+	};
+
 	return (
 		<div className="Home">
-			<p>
-				Hello from the home (<code>/</code>) page!
-			</p>
+			<Button label={'Create New List'} onClick={createNewList} />
+			<p>-or-</p>
+			<p>Join an existing shopping list by entering a three word token.</p>
+
+			<form>
+				<label htmlFor="token">Share token</label>
+				<input
+					type="text"
+					name="token"
+					onChange={handleTokenInput}
+					value={retrievedToken}
+				/>
+
+				<Button
+					label="Join an existing list"
+					type="submit"
+					onClick={joinExistentList}
+				/>
+			</form>
 		</div>
 	);
 }
