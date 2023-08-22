a2e4497b00974fb0ef060813d51bbdd6c0a30a41
Aslan Taheri <fromthenarnia@gmail.com>
2023-08-22 19:15:21 -0400

    Add token display and propmt for empty list notification.

--- src/views/List.jsx
+++ src/views/List.jsx
@@ -1,11 +1,17 @@
 import { ListItem } from '../components';
 
 export function List({ data }) {
+	const token = localStorage.getItem('tcl-shopping-list-token');
+	console.log(token);
 	return (
 		<>
 			<p>
 				Hello from the <code>/list</code> page!
 			</p>
+			<p>Your token is: {token} </p>
+			{data.length === 0 && (
+				<p>Your shopping list is empty. Click on "Add item" to begin!</p>
+			)}
 			<ul>
 				{data.map((item) => (
 					<ListItem key={item.id} name={item.name} />
