2075d35744bdec4d64d979191c780efb655006ff
Aslan Taheri <fromthenarnia@gmail.com>
2023-08-25 12:29:51 -0400

    Refactor and clean up out of scope features and comments.

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,38 +1,13 @@
 import { generateToken } from '@the-collab-lab/shopping-list-utils';
 import Button from '../components/Button';
 import './Home.css';
-import { useState } from 'react';
-import { createCollection } from '../api/firebase';
 
 export function Home({ setListToken }) {
-	const [retrievedToken, setRetrievedToken] = useState('');
+	//This function generates a new unique Token corresponding to a new shopping list.
 
-	/**
-	 * This function generates a new unique Token corresponding to a new shopping list.
-	 */
 	const createNewList = () => {
 		const newToken = generateToken();
-		setRetrievedToken(newToken);
-
-		/**
-		 * We were creating a new collection in the database using the newToken:
-		 * createCollection(newToken);
-		 */
-	};
-
-	/**
-	 *  This function ensures that the retrievedToken state always reflects the current value in the input field.
-	 */
-	const handleTokenInput = (e) => {
-		setRetrievedToken(e.target.value);
-	};
-
-	/**
-	 * This function attempts to join a shopping list based on the provided Token.
-	 */
-	const joinExistentList = (e) => {
-		e.preventDefault();
-		setListToken(retrievedToken);
+		setListToken(newToken);
 	};
 
 	return (
@@ -43,18 +18,9 @@ export function Home({ setListToken }) {
 
 			<form>
 				<label htmlFor="token">Share token</label>
-				<input
-					type="text"
-					name="token"
-					onChange={handleTokenInput}
-					value={retrievedToken}
-				/>
+				<input type="text" name="token" />
 
-				<Button
-					label="Join an existing list"
-					type="submit"
-					onClick={joinExistentList}
-				/>
+				<Button label="Join an existing list" type="submit" />
 			</form>
 		</div>
 	);
