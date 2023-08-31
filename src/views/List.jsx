98adc856cd67abd6d24dceb4329a2a74ef6ddc6d
rdpcodes <54380958+rdpfeifle@users.noreply.github.com>
2023-08-30 22:31:29 -0600

    Remove some unused code

--- src/views/List.jsx
+++ src/views/List.jsx
@@ -1,31 +1,24 @@
 import { ListItem } from '../components';
 
 export function List({ data, listToken }) {
-
-    console.log(data);
-    // OPTION B - GOES WITH OPTION B IN Firebase.js and OPTION B IN Home.jsx
+	// OPTION B - GOES WITH OPTION B IN Firebase.js and OPTION B IN Home.jsx
 	// FILTER OUT THE DATA THAT CONTAINS AN OBJECT WITH NO NAME PROPERTY
-    const realData = data.filter((item) => item.name);
-    
+	const realData = data.filter((item) => item.name);
 
-    return (
-        <>
-            <p>
-                Hello from the <code>/list</code> page!
-            </p>
-            <p>Your token is: {listToken}</p>
+	return (
+		<>
+			<p>Your token is: {listToken}</p>
 			{/* PART OF OPTION B. OTHERWISE USE data.length FOR OPTION A*/}
-            {realData.length === 0 && (
-                <p>Your shopping list is empty. Click on "Add item" to begin!</p>
-            )}
-            <ul>
+			{realData.length === 0 && (
+				<p>Your shopping list is empty. Click on "Add item" to begin!</p>
+			)}
+			<ul>
 				{/* PART OF OPTION B. OTHERWISE USE data.map FOR OPTION A*/}
 				{/* POSSIBLE CONFLICT WITH ISSUE #6 */}
-                {realData.map((item) => (
-                    <ListItem key={item.id} name={item.name} />
-                ))}
-            </ul>
-        </>
-    );
+				{realData.map((item) => (
+					<ListItem key={item.id} name={item.name} />
+				))}
+			</ul>
+		</>
+	);
 }
-
