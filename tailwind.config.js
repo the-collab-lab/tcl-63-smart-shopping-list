b352ab66063682e5e2846aeb0f1f8f2b1f0aa6ff
rdpcodes <54380958+rdpfeifle@users.noreply.github.com>
2023-09-27 14:09:31 -0600

    Fix tailwind install bug

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{js,jsx,ts,tsx}'],
+	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
 		extend: {},
 	},
-	plugins: [require('tailwindcss'), require('daisyui')],
+	plugins: [require('daisyui')],
 	daisyui: {
 		themes: ['pastel', 'emerald'],
 	},
