98ec2607496cfebb094f0f98ff68816281999d70
Up Crown <wangshangguan@hotmail.com>
2023-09-29 11:40:48 -0500

    hide Navbar when there is no list token

--- src/views/Layout.jsx
+++ src/views/Layout.jsx
@@ -27,43 +27,46 @@ export function Layout({ listToken }) {
 				<main className="Layout-main">
 					<Outlet />
 				</main>
-				<nav className="Nav bg-red-100">
-					<div className="Nav-container">
-						{/* this removes the Home navlink from the List and AddItem pages when there is a token assigned */}
-						{!listToken && (
+				{/* remove navbar from Home when there is no list token */}
+				{listToken && (
+					<nav className="Nav bg-red-100">
+						<div className="Nav-container">
+							{/* this removes the Home navlink from the List and AddItem pages when there is a token assigned */}
+							{!listToken && (
+								<NavLink
+									to="/"
+									className={`Nav-link text-slate-950 hover:text-slate-700 ${
+										isActive['home'] &&
+										'font-bold underline decoration-red-400 underline-offset-2'
+									}`}
+									onClick={() => handleNavLinkClick('home')}
+								>
+									Home
+								</NavLink>
+							)}
 							<NavLink
-								to="/"
+								to="/list"
 								className={`Nav-link text-slate-950 hover:text-slate-700 ${
-									isActive['home'] &&
+									isActive['list'] &&
 									'font-bold underline decoration-red-400 underline-offset-2'
 								}`}
-								onClick={() => handleNavLinkClick('home')}
+								onClick={() => handleNavLinkClick('list')}
 							>
-								Home
+								List
 							</NavLink>
-						)}
-						<NavLink
-							to="/list"
-							className={`Nav-link text-slate-950 hover:text-slate-700 ${
-								isActive['list'] &&
-								'font-bold underline decoration-red-400 underline-offset-2'
-							}`}
-							onClick={() => handleNavLinkClick('list')}
-						>
-							List
-						</NavLink>
-						<NavLink
-							to="/add-item"
-							className={`Nav-link text-slate-950 hover:text-slate-700 ${
-								isActive['addItem'] &&
-								'font-bold underline decoration-red-400 underline-offset-2'
-							}`}
-							onClick={() => handleNavLinkClick('addItem')}
-						>
-							Add Item
-						</NavLink>
-					</div>
-				</nav>
+							<NavLink
+								to="/add-item"
+								className={`Nav-link text-slate-950 hover:text-slate-700 ${
+									isActive['addItem'] &&
+									'font-bold underline decoration-red-400 underline-offset-2'
+								}`}
+								onClick={() => handleNavLinkClick('addItem')}
+							>
+								Add Item
+							</NavLink>
+						</div>
+					</nav>
+				)}
 			</div>
 		</>
 	);
