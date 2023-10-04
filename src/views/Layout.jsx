7b9c493bd7dc33a02a3290c1192e8b7b2443eb82
Up Crown <wangshangguan@hotmail.com>
2023-10-04 17:33:10 -0500

    separate div of main with navbar and add a mt to navbar

--- src/views/Layout.jsx
+++ src/views/Layout.jsx
@@ -6,7 +6,7 @@ import logo from '../assets/logo-bluebg.png';
 export function Layout({ listToken }) {
 	return (
 		<>
-			<div className="Layout flex flex-col overflow-y-clip h-screen my-auto">
+			<div className="Layout flex flex-col overflow-y-clip h-screen">
 				<header className="flex justify-center items-center mt-2">
 					<img
 						src={logo}
@@ -18,35 +18,35 @@ export function Layout({ listToken }) {
 					<Outlet />
 				</main>
 				<div className='absolute z-0 right-0 bg-right bg-[url("../../img/berry-bg.png")] bg-cover inset-0' />
-				{/* remove navbar from Home when there is no list token */}
-				{listToken && (
-					<nav className="z-5 flex fixed py-1 md:py-2 lg:py-4 bottom-0 w-full justify-center bg-red-100">
-						<div className="flex gap-20">
-							{/* this removes the Home navlink from the List and AddItem pages when there is a token assigned */}
-							{!listToken && (
-								<NavLink
-									to="/"
-									className="Nav-link text-slate-950 hover:text-slate-700"
-								>
-									Home
-								</NavLink>
-							)}
-							<NavLink
-								to="/list"
-								className="Nav-link text-slate-950 hover:text-slate-700"
-							>
-								List
-							</NavLink>
-							<NavLink
-								to="/add-item"
-								className="Nav-link text-slate-950 hover:text-slate-700"
-							>
-								Add Item
-							</NavLink>
-						</div>
-					</nav>
-				)}
 			</div>
+			{/* remove navbar from Home when there is no list token */}
+			{listToken && (
+				<nav className="z-5 flex fixed py-1 md:py-2 lg:py-4 bottom-0 w-full justify-center bg-red-100 mt-5">
+					<div className="flex gap-20">
+						{/* this removes the Home navlink from the List and AddItem pages when there is a token assigned */}
+						{!listToken && (
+							<NavLink
+								to="/"
+								className="Nav-link text-slate-950 hover:text-slate-700"
+							>
+								Home
+							</NavLink>
+						)}
+						<NavLink
+							to="/list"
+							className="Nav-link text-slate-950 hover:text-slate-700"
+						>
+							List
+						</NavLink>
+						<NavLink
+							to="/add-item"
+							className="Nav-link text-slate-950 hover:text-slate-700"
+						>
+							Add Item
+						</NavLink>
+					</div>
+				</nav>
+			)}
 		</>
 	);
 }
