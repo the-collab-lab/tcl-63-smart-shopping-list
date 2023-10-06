6067bb541da62ffb21849b7351141d0e2763dfdf
rdpcodes <54380958+rdpfeifle@users.noreply.github.com>
2023-10-06 10:55:25 -0600

    Improve mobile responsiveness of About page

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -20,24 +20,30 @@ export const About = ({ listToken }) => {
 						<ArrowLeftIcon className="h-10 w-10 hover:text-red-400" />
 					</NavLink>
 				)}
-				<h1 className="font-bold text-4xl mb-6">How BerryCart Works</h1>
-				<section className="chat chat-start centered-section pb-6">
-					<p className="chat-bubble chat-bubble-secondary w-1/2">
+				<h1 className="font-bold text-4xl mb-6 max-md:mt-6">
+					How BerryCart Works
+				</h1>
+				<section className="chat md:chat-start centered-section pb-6">
+					<p className="chat-bubble chat-bubble-secondary md:w-1/2">
 						BerryCart is a personalized shopping list. You can manage your
 						grocery items in one app.
 					</p>
 				</section>
-				<div className="lg:flex justify-center items-center mt-2">
-					<img src={homeImg} alt="home page mockup" className="h-[500px]" />
-					<section className="chat chat-start">
+				<div className="flex lg:flex-row max-md:flex-col-reverse justify-center items-center">
+					<img
+						src={homeImg}
+						alt="home page mockup"
+						className="h-[500px] max-md:h-[430px]"
+					/>
+					<section className="chat chat-start max-md:mb-2">
 						<p className="chat-bubble chat-bubble-secondary">
 							Start by creating a new list, or enter your three-word token to
 							access an existing shopping list.
 						</p>
 					</section>
 				</div>
-				<div className="lg:flex justify-center items-center mt-5">
-					<section className="chat chat-end pr-10">
+				<div className="flex flex-col md:flex-row justify-center items-center mt-5">
+					<section className="chat chat-end max-md:chat-start pr-10">
 						<p className="chat-bubble chat-bubble-secondary">
 							You can easily add a new item to the list, and choose how soon you
 							will buy this item again.{' '}
@@ -46,11 +52,15 @@ export const About = ({ listToken }) => {
 					<img
 						src={addItemImg}
 						alt="add item page mockup"
-						className="h-[500px] mt-2"
+						className="h-[500px] max-md:h-[430px] mt-2"
 					/>
 				</div>
-				<div className="lg:flex justify-center items-center">
-					<img src={listImg} alt="list page mockup" className="h-[500px]" />
+				<div className="flex lg:flex-row max-md:flex-col-reverse justify-center items-center">
+					<img
+						src={listImg}
+						alt="list page mockup"
+						className="h-[500px] max-md:h-[430px]"
+					/>
 					<div className="flex flex-col gap-7">
 						<section className="chat chat-start">
 							<p className="chat-bubble chat-bubble-secondary">
@@ -58,7 +68,7 @@ export const About = ({ listToken }) => {
 								an item as purchased by checking it off from the list.{' '}
 							</p>
 						</section>
-						<section className="chat chat-start">
+						<section className="chat chat-start max-md:mb-2">
 							<p className="chat-bubble chat-bubble-secondary">
 								BerryCart will learn your purchasing frequency and estimate how
 								soon you will likely need this item again.
